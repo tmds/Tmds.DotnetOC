53d05e629552e4efa219c9d8d8903b1e2fc8a2ab
Tom Deseyn <tom.deseyn@gmail.com>
2018-06-28 09:32:07 +0200

    Add Tmds.DotnetOC project

---
+++ src/Tmds.DotnetOC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace Tmds.DotnetOC
+{
+    [Command(Name = "dotnet-oc")]
+    class Program
+    {
+        public static int Main(string[] args)
+            => CommandLineApplication.Execute<Program>(args);
+
+        int OnExecute(CommandLineApplication app)
+        {
+            app.ShowHelp();
+            return 0;
+        }
+    }
+}
